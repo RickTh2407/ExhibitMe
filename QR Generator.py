f0f4ee9026e66e63d827b31b0044e43a70447cb2
RickTh2407 <101653871+RickTh2407@users.noreply.github.com>
2023-01-27 19:17:38 +0100

    Re-upload 27-01-2023 Hotfix2
    *Fix for QR generator where JSON file communication was shit
    * Added png's due to fix QR generator
    * Added underlining to explicit negative word in the NO-answers of the register
    * More fixes coming up

--- QR Generator.py
+++ QR Generator.py
@@ -11,33 +11,42 @@ with open("guestDatabase.json", "r") as f:
     gData = json.load(f)
 
 # iterate through the guests
-for guest in gData["Guests"]:
-    for guestNr in guest:
+def JGD(): #stands for Json Guest Details
+    for guestNr in gData["Guests"].values():
     # extract the information per guest
-        fName = guestNr["FirstName/CompanyName"]
-        lName = guestNr["LastName"]
-        age = guestNr["Age"]
-        email = guestNr["Email"]
-        adressStr = guestNr["AdressStreet"]
-        adressNr = guestNr["AdressHouseNr"]
-        phone = guestNr["PhoneNr"]
-        if (guestNr["ShowResidence"] == "False") or (adressStr == ""):
+        fName = guestNr.get("FirstName/CompanyName")
+        lName = guestNr.get("LastName")
+        age = guestNr.get("Age")
+        gender = guestNr.get("Gender")
+        addressStr = guestNr.get("AddressStreet")
+        addressNr = guestNr.get("AddressHouseNr")
+        addressZip = guestNr.get("AddressZip")
+        phone = guestNr.get("PhoneNr")
+        email = guestNr.get("Email")
+        if (guestNr.get("ShowResidence") == "False") or (addressStr == ""):
             adressStr = "Classified/Unknown"
             adressNr = ""
-        if (guestNr["ShowPhone"] == "False") or (phone == ""):
+        if (guestNr.get("ShowPhone") == "False") or (phone == ""):
             phone = "Classified/Unknown"
-        if (guestNr["ShowEmail"] == "False") or (email == ""):
+        if (guestNr.get("ShowEmail") == "False") or (email == ""):
             email = "Classified/Unknown"
-        if (guestNr["UseAge"] == "False") or (age == ""):
+        if (guestNr.get("UseAge") == "False") or (age == ""):
             age = "Classified/Unknown"
+        
+        # create the QR code
+        qr = qrcode.QRCode(version=1, box_size=10, border=5)
+        qr.add_data(f"""Name: {fName} + " " + {lName}
+        Age: {age}
+        Gender: {gender}
+        Address: {addressStr} + {addressNr}
+        Zip: {addressZip}
+        Phone: {phone}
+        Email: {email}""")
+        qr.make(fit=True)
 
-    # create the QR code
-    qr = qrcode.QRCode(version=1, box_size=10, border=5)
-    qr.add_data(f"Name: {fName} + {lName}\nAge: {age}\nEmail: {email}\nAdress: {adressStr} + {adressNr}\nPhone: {phone}")
-    qr.make(fit=True)
+        # create an image from the QR code
+        img = qr.make_image(fill_color="black", back_color="white")
 
-    # create an image from the QR code
-    img = qr.make_image(fill_color="black", back_color="white")
-
-    # join the directory and file name and save the image
-    img.save(os.path.join("qr_codes", f"{fName[0]} + {lName}.png", sep="_"))
+        # join the directory and file name and save the image
+        img.save(os.path.join("qr_codes", f"{fName[0]}{lName}.png"))
+JGD()
